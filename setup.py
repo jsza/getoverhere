4da04bba9b1a83a0ed06c517055feadc80760bd5
jayess <jayess@jayess.za.net>
2014-11-07 01:37:28 +0200

    Rename script to 'goh'.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'getoverhere',
     version = '0.1',
     packages = find_packages(),
-    scripts = ['scripts/getoverhere'],
+    scripts = ['scripts/goh'],
     install_requires = ['beautifulsoup4', 'requests'],
     author = 'jayess',
     description = 'Addon updater/installer for source engine games.',

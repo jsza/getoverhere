d671d4f6b70091f952a8399c940ba2dbfd8bade3
jayess <jayess@jayess.za.net>
2014-11-07 01:14:14 +0200

    Add setup.py and restructure project.

--- getoverhere.py
+++ getoverhere.py
@@ -1,27 +1,33 @@
 from argparse import ArgumentParser
-from downloader import Downloader
+from getoverhere.downloader import Downloader
 
 
 
-parser = ArgumentParser()
-parser.add_argument('path',
-                    help='Target path on disk.')
-parser.add_argument('deployments', nargs='*', help='Deployments.')
-parser.add_argument('-fi', '--full-install',
-                    help='Overwrite all files.',
-                    action='store_true')
-parser.add_argument('-sc', '--skip-confirm',
-                    help='Skip confirmation for downloading files.',
-                    action='store_true')
-parser.add_argument('-fu', '--force-update',
-                    help='Force install if already up-to-date. '
-                         'This should not be needed unless something went horribly wrong.',
-                    action='store_true')
-args = parser.parse_args()
-basePath = args.path
-fullInstall = args.full_install
-skipConfirm = args.skip_confirm
-forceUpdate = args.force_update
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('path',
+                        help='Target path on disk.')
+    parser.add_argument('deployments', nargs='*', help='Deployments.')
+    parser.add_argument('-fi', '--full-install',
+                        help='Overwrite all files.',
+                        action='store_true')
+    parser.add_argument('-sc', '--skip-confirm',
+                        help='Skip confirmation for downloading files.',
+                        action='store_true')
+    parser.add_argument('-fu', '--force-update',
+                        help='Force install if already up-to-date. '
+                             'This should not be needed unless something went horribly wrong.',
+                        action='store_true')
+    args = parser.parse_args()
+    basePath = args.path
+    fullInstall = args.full_install
+    skipConfirm = args.skip_confirm
+    forceUpdate = args.force_update
 
-for deployment in args.deployments:
-    Downloader(deployment, basePath, fullInstall, skipConfirm, forceUpdate).download()
+    for deployment in args.deployments:
+        Downloader(deployment, basePath, fullInstall, skipConfirm, forceUpdate).download()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'getoverhere',
+    version = '0.1',
+    packages = find_packages(),
+    scripts = ['getoverhere.py'],
+    install_requires = ['beautifulsoup4', 'requests', 'whatever'],
+    author = 'jayess',
+    description = 'Addon updater/installer for source engine games.',
+    url = 'https://github.com/jsza/getoverhere'
+)

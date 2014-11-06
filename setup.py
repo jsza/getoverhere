3bbf8e97dbf5729944948651eef3bd3300d850fa
jayess <jayess@jayess.za.net>
2014-11-07 01:33:40 +0200

    Remove bogus dependancy.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '0.1',
     packages = find_packages(),
     scripts = ['scripts/getoverhere'],
-    install_requires = ['beautifulsoup4', 'requests', 'whatever'],
+    install_requires = ['beautifulsoup4', 'requests'],
     author = 'jayess',
     description = 'Addon updater/installer for source engine games.',
     url = 'https://github.com/jsza/getoverhere'
